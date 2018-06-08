8b608a2d08be10abbc955c5645a5caa753785697
Albert <alberto.abad.sanz@gmail.com>
2018-06-08 16:26:38 +0200

    Tonteria

--- Motor_Alb_Jano/Source/GameEngine/Texture.cpp
+++ Motor_Alb_Jano/Source/GameEngine/Texture.cpp
@@ -123,7 +123,7 @@ void LTexture::setBlendMode(SDL_BlendMode blending)
 void LTexture::setAlpha(Uint8 alpha)
 {
   //Modulate texture alpha
-  SDL_SetTextureAlphaMod(mTexture, alpha);
+  SDL_SetTextureAlphaMod(mTexture, alpha);//A
 }
 
 void LTexture::render(int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
